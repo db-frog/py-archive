6987c53f7fee5f91ec25527ea894f0143ad9e4f9
Daniel Benett <deb2174@columbia.edu>
2024-12-18 12:21:17 -0800

    Allow CORS

--- app/main.py
+++ app/main.py
@@ -1,6 +1,7 @@
 from contextlib import asynccontextmanager
 
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from pymongo import MongoClient
 from routes import router as folklore_router
 import boto3
@@ -15,5 +16,17 @@ async def lifespan(app: FastAPI):
     app.mongodb_client.close()
 
 app = FastAPI(lifespan=lifespan)
+origins = [
+    "https://docker15547-env-7928981.us.reclaim.cloud/",
+    "https://env-7928981.us.reclaim.cloud/",
+    "http://localhost",
+]
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 app.include_router(folklore_router, tags=["folklore"], prefix="/folklore")
